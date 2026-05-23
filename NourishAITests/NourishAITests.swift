e9cb5a108e5017296f0473e0d31604860ddcdd6c
mcagridemir <cagriidemirr@gmail.com>
2026-05-23 16:41:25 +0300

    Initial Commit

---
+++ NourishAITests/NourishAITests.swift
@@ -0,0 +1,17 @@
+//
+//  NourishAITests.swift
+//  NourishAITests
+//
+//  Created by cagri.demir on 23.05.2026.
+//
+
+import Testing
+@testable import NourishAI
+
+struct NourishAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
